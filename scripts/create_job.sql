88c219829839762509b25a5fb4391e79e3577b5a
Heiner-2004 <heiner.vallejo@udea.edu.co>
2025-09-21 17:12:48 -0500

    Update create_job.sql

--- scripts/create_job.sql
+++ scripts/create_job.sql
@@ -26,3 +26,41 @@ HAVING MAX(Orders.OrderDate) < DATEADD(DAY, -183, GETDATE())
        );
 END;
 GO
+/*=================================*/
+
+-- Crear el job
+EXEC msdb.dbo.sp_add_job
+    @job_name = 'Job_LogInactiveCustomers',
+    @enabled = 1,
+    @description = 'Job que detecta clientes inactivos y los inserta en InactiveCustomersLog';
+GO
+
+-- Crear un paso que ejecute el procedimiento
+EXEC msdb.dbo.sp_add_jobstep
+    @job_name = 'Job_LogInactiveCustomers',
+    @step_name = 'Detectar clientes inactivos',
+    @subsystem = 'TSQL',
+    @database_name = 'Northwind2025',
+    @command = 'EXEC sp_LogInactiveCustomers;',
+    @on_success_action = 1;
+GO
+
+-- Crear un schedule (ejemplo: todos los días a las 02:00 AM)
+EXEC msdb.dbo.sp_add_schedule
+    @schedule_name = 'Daily_LogInactiveCustomers',
+    @freq_type = 4, -- Diario
+    @freq_interval = 1,
+    @active_start_time = 020000; -- 02:00 AM
+GO
+
+-- Asociar el schedule al job
+EXEC msdb.dbo.sp_attach_schedule
+    @job_name = 'Job_LogInactiveCustomers',
+    @schedule_name = 'Daily_LogInactiveCustomers';
+GO
+
+-- Asociar el job al servidor actual
+EXEC msdb.dbo.sp_add_jobserver
+    @job_name = 'Job_LogInactiveCustomers';
+GO
+>>>>>>> aca667c7e22f170d3b58d7524f924106aa56288f
